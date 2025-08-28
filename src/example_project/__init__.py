b3c4b8cce7922771f77b5b2577d756c7543297ba
Justin Flannery <justin.flannery@juftin.com>
2025-08-27 22:56:44 -0600

    ✨ initial commit

---
+++ src/example_project/__init__.py
@@ -0,0 +1,10 @@
+"""
+example-project
+"""
+
+from example_project.__about__ import __application__, __version__
+
+__all__ = [
+    "__application__",
+    "__version__",
+]
---
+++ tests/test_example.py
@@ -0,0 +1,12 @@
+"""
+Example test case for example-project.
+"""
+
+from example_project import __application__
+
+
+def test_example() -> None:
+    """
+    Just a simple test case.
+    """
+    assert __application__ == "example-project"
